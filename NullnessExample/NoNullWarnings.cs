2a7aee683d2b7b06bd7d300ee6e68c54b013254f
Phillip Carter <phcart@microsoft.com>
2019-05-15 08:15:13 -0700

    Add project files.

---
+++ NullnessExample/NoNullWarnings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullnessExample
+{
+    class NoNulls
+    {
+        public int Len(string? s) => s.Length;
+    }
+}
---
+++ NullnessExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+#nullable enable
+
+namespace NullnessExample
+{
+    class Program
+    {
+        public void M(string? theString)
+        {
+            Console.WriteLine($"Length is {theString.Length}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
