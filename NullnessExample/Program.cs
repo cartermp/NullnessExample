9eb4365b57fd0896e1cb128110d81b92f60d46c1
Phillip Carter <phcart@microsoft.com>
2019-05-15 10:19:28 -0700

    Show how NoWarn doesn't work (yet).

--- NullnessExample/Program.cs
+++ NullnessExample/Program.cs
@@ -8,10 +8,14 @@ namespace NullnessExample
     {
         public void M(string? theString)
         {
+            // In the project file, this warning code is suppressed, but the nowarn is ignored.
             Console.WriteLine($"Length is {theString.Length}");
         }
         static void Main(string[] args)
         {
+            // Explicitly trigger a warning here, note that is gets suppressed
+            while (false) { Console.Write(""); }
+
             Console.WriteLine("Hello World!");
         }
     }
